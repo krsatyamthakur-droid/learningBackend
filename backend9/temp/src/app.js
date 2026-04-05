52350fe2cd486696ee64c46da4c420f17885b721
satyam kumar <krsatyamthakur@gmail.com>
2026-04-05 23:27:23 +0530

    routes

---
+++ backend9/temp/src/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from'cors';
+import cookieparsar from 'cookie-parser';
+import { p } from 'framer-motion/client';
+const app=express();
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}));
+app.use(express.json({limit:"10mb"}));
+app.use(express.urlencoded({extended:true,limit:"10mb"}))
+app.use(express.static("public"))
+app.use(cookieparser())
+
+
+// routes
+
+import {router} from '../routes/user.routes.js';
+
+// routes decleration
+ app.use("/users/",router)
+
+export {app}
