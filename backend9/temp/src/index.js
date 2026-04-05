016e69949258a39aa6d7004975b8811e7ca376e9
satyam kumar <krsatyamthakur@gmail.com>
2026-04-05 23:27:23 +0530

    routes

---
+++ backend9/temp/src/index.js
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import app from "./app.js";
+
+dotenv.config({
+  path: "./.env"
+});
+
+connectDB()
+//   .then(() => {
+//     app.on("error", (error) => {
+//       console.error("Express error:", error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT || 8000, () => {
+//       console.log(`Server is running on port ${process.env.PORT || 8000}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.log("MONGOOSE connection failed:", error);
+//   });
+
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("error",error);
+        throw error 
+    })
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(`server is running on port ${process.env.PORT || 8000}`);
+  })
+})
+.catch((error)=>{
+ console.log("MONGOOSE connection failes:",error);
+})
---
+++ backend9/temp/src/utils/asynchandler.js
@@ -0,0 +1,28 @@
+const asyncHandler=(requestHandler)=>{
+  return (req,res,next) => {
+     Promise.resolve(requestHandler(req,res,next)).
+     catch((err) => next(err))
+   }
+
+}
+export  {asyncHandler};
+
+
+
+// const asynchandler = () =>{}
+// const asynchandler= (func) => () => {}
+// const asynchandler=(func) => async() =>{}
+
+
+
+
+// const asynchandler= (func) => async(req,res,next) => {
+//     try{
+//     await func(req,res,next)
+//     } catch(error){
+//         res.status(err.code || 500).json({
+//             success:false,
+//             message:err.message
+//         })
+//     }
+// }
